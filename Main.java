60dad1d66138c4ef3aa7cc7d11cd3e9eb8f45bca
alt1983 <timofeev_a_v@mail.ru>
2022-05-08 00:56:34 +0300

    Commit 1

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        //Возникает ошибка деления на 0
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
+
+    }
+}
